221c94b4f5bebd73a2c4ac37e89a06591fee8dd1
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2023-06-24 12:20:34 +0200

    First Version Fixes #1

--- pages/index.ts
+++ pages/index.ts
@@ -1,15 +1,1 @@
 import { Result, Error, Valid } from "../src"
-
-
-let test = Valid(5) as Result<number, string>;
-let test2 = test.andThen((value) => {
-    return Valid(6);
-})
-let test3 = test.map((errer) => {
-    return Error(5)
-})
-
-console.log((await test2).unwrap());
-
-
-let test5 = await Promise.race([fetch('URL'), new Promise((a) => { setTimeout(a, 3000, false) })])
