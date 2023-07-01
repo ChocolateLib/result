12d7bae6bf68e38e6346e8d30f720843f5c758dc
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2023-07-01 18:44:21 +0200

    Add better support for async result returns Fixes #3

--- pages/index.ts
+++ pages/index.ts
@@ -1,8 +1,59 @@
-import { Result, Error, Valid } from "../src"
 
+interface ResultBase<T, E> {
+    readonly valid: boolean;
+    readonly ok: boolean;
+    readonly err: boolean;
+    readonly value?: T;
+    readonly error?: E;
+}
 
+class ResultValid<T> implements ResultBase<T, never> {
+    readonly value: T
+    constructor(value: T) { this.value = value; }
+    get valid(): true { return true }
+    get ok(): true { return true }
+    get err(): false { return false }
+}
 
+class ResultError<E> implements ResultBase<never, E> {
+    readonly error: E
+    #stack: string | undefined = new Error().stack;
+    constructor(error: E) { this.error = error; }
+    get valid(): false { return false }
+    get ok(): false { return false }
+    get err(): true { return true }
+}
 
+type Result<T, E> = ResultValid<T> | ResultError<E>;
 
+type ResultExecutor<T, E> = (valid: (value: ResultValid<T>) => void, error: (error: ResultError<E>) => void) => void;
 
-console.log(await test.sync());
+class ResultAsync<T, E> extends Promise<Result<T, E>> {
+    constructor(executor: ResultExecutor<T, E>) {
+        super(executor)
+    }
+}
+
+let test1 = (): Result<number, string> => {
+    if (Math.random() > 0.5) {
+        return new ResultValid(789);
+    } else {
+        return new ResultError('Fail');
+    }
+}
+let yo1 = test1()
+
+let test2 = (): ResultAsync<number, string> => {
+    return new ResultAsync((valid, error) => {
+        if (Math.random() > 0.5) {
+            valid(new ResultValid(789));
+        } else {
+            error(new ResultError('Fail'));
+        }
+    });
+}
+let yo2 = test2();
+(async () => {
+    let yo3 = await yo2;
+
+})()
--- src/async.ts
+++ src/async.ts
@@ -1,147 +1,147 @@
-import { Optional, Result } from "./index";
-import { ResultError } from "./error";
-import { ResultValid } from "./valid";
-import { ResultBaseActions } from "./base";
+// import { Optional, Result } from "./index";
+// import { ResultError } from "./error";
+// import { ResultValid } from "./valid";
+// import { ResultBaseActions } from "./base";
 
 
-export class ResultAsync<T, E> extends Promise<T> implements ResultBaseActions<T, E> {
-    constructor(executor: (valid: (value: T) => void, error: (value: E) => void) => void) {
-        super(executor);
-    }
+// export class ResultAsync<T, E> extends Promise<T> {
+//     constructor(executor: (valid: (value: T) => void, error: (value: E) => void) => void) {
+//         super(executor);
+//     }
 
-    async sync() {
-        try {
-            return new ResultValid(await this);
-        } catch (e) {
-            return new ResultError(e);
-        }
-    }
+//     async sync() {
+//         try {
+//             return new ResultValid(await this);
+//         } catch (e) {
+//             return new ResultError(e);
+//         }
+//     }
 
-    get valid(): true {
-        return true
-    }
-    get ok(): true {
-        return true
-    }
-    get err(): false {
-        return false
-    }
+//     async expect(): Promise<T> {
+//         return this
+//     }
 
-    expect(): T {
-        return this.value
-    }
+//     async expectErr(msg: string): Promise<E> {
+//         try {
+//             await this;
+//             throw (msg);
+//         } catch (e) {
+//             return e as any
+//         }
+//     }
 
-    expectErr(msg: string): never {
-        throw new Error(msg);
-    }
+//     unwrap(): Promise<T> {
+//         return this
+//     }
 
-    unwrap(): T {
-        return this.value
-    }
+//     unwrapOr<T2>(val: T2): T2 {
+//         try {
+            
+//         } catch (error) {
+            
+//         }
+//         return this.value
+//     }
 
-    unwrapOr(): T {
-        return this.value
-    }
+//     andThen<T2>(mapper: (value: T) => ResultValid<T2>): ResultValid<T2>;
+//     andThen<E2>(mapper: (value: T) => ResultError<E2>): ResultError<E2>;
+//     andThen<T2, E2>(mapper: (value: T) => Result<T2, E2>): Result<T2, E2> {
+//         return mapper(this.value);
+//     }
+//     andThenAsync<T2>(mapper: (value: T) => Promise<ResultValid<T2>>): Promise<ResultValid<T2>>;
+//     andThenAsync<E2>(mapper: (value: T) => Promise<ResultError<E2>>): Promise<ResultError<E2>>;
+//     andThenAsync<T2, E2>(mapper: (value: T) => Promise<Result<T2, E2>>): Promise<Result<T2, E2>> {
+//         return mapper(this.value);
+//     }
 
-    andThen<T2>(mapper: (value: T) => ResultValid<T2>): ResultValid<T2>;
-    andThen<E2>(mapper: (value: T) => ResultError<E2>): ResultError<E2>;
-    andThen<T2, E2>(mapper: (value: T) => Result<T2, E2>): Result<T2, E2> {
-        return mapper(this.value);
-    }
-    andThenAsync<T2>(mapper: (value: T) => Promise<ResultValid<T2>>): Promise<ResultValid<T2>>;
-    andThenAsync<E2>(mapper: (value: T) => Promise<ResultError<E2>>): Promise<ResultError<E2>>;
-    andThenAsync<T2, E2>(mapper: (value: T) => Promise<Result<T2, E2>>): Promise<Result<T2, E2>> {
-        return mapper(this.value);
-    }
+//     orElse(): ResultValid<T> {
+//         return this;
+//     }
+//     async orElseAsync(): Promise<ResultValid<T>> {
+//         return this;
+//     }
 
-    orElse(): ResultValid<T> {
-        return this;
-    }
-    async orElseAsync(): Promise<ResultValid<T>> {
-        return this;
-    }
+//     map<U>(func: (value: T) => U): ResultValid<U> {
+//         return new ResultValid(func(this.value));
+//     }
+//     async mapAsync<U>(func: (value: T) => Promise<U>): Promise<ResultValid<U>> {
+//         return new ResultValid(await func(this.value));
+//     }
 
-    map<U>(func: (value: T) => U): ResultValid<U> {
-        return new ResultValid(func(this.value));
-    }
-    async mapAsync<U>(func: (value: T) => Promise<U>): Promise<ResultValid<U>> {
-        return new ResultValid(await func(this.value));
-    }
+//     mapErr(): ResultValid<T> {
+//         return this
+//     }
+//     async mapErrAsync(): Promise<ResultValid<T>> {
+//         return this
+//     }
 
-    mapErr(): ResultValid<T> {
-        return this
-    }
-    async mapErrAsync(): Promise<ResultValid<T>> {
-        return this
-    }
+//     toOptional(): OptionalSome<T> {
+//         return new OptionalSome(this.value);
+//     }
 
-    toOptional(): OptionalSome<T> {
-        return new OptionalSome(this.value);
-    }
+//     /**Returns the contained valid value, but never throws.
+//      * Unlike `unwrap()`, this method doesn't throw and is only callable on an Ok<T>
+//      * Therefore, it can be used instead of `unwrap()` as a maintainability safeguard
+//      * that will fail to compile if the error type of the Result is later changed to an error that can actually occur.*/
+//     safeUnwrap(): T {
+//         return this.value
+//     }
+// }
 
-    /**Returns the contained valid value, but never throws.
-     * Unlike `unwrap()`, this method doesn't throw and is only callable on an Ok<T>
-     * Therefore, it can be used instead of `unwrap()` as a maintainability safeguard
-     * that will fail to compile if the error type of the Result is later changed to an error that can actually occur.*/
-    safeUnwrap(): T {
-        return this.value
-    }
-}
+// export class OptionalSome<T> implements OptionalBase<T> {
+//     readonly value: T
 
-export class OptionalSome<T> implements OptionalBase<T> {
-    readonly value: T
+//     constructor(value: T) {
+//         this.value = value;
+//     }
+//     get valid(): true {
+//         return true
+//     }
+//     get some(): true {
+//         return true
+//     }
+//     get none(): false {
+//         return false
+//     }
 
-    constructor(value: T) {
-        this.value = value;
-    }
-    get valid(): true {
-        return true
-    }
-    get some(): true {
-        return true
-    }
-    get none(): false {
-        return false
-    }
+//     expect(): T {
+//         return this.value;
+//     }
 
-    expect(): T {
-        return this.value;
-    }
+//     unwrap(): T {
+//         return this.value;
+//     }
 
-    unwrap(): T {
-        return this.value;
-    }
+//     unwrapOr(): T {
+//         return this.value;
+//     }
 
-    unwrapOr(): T {
-        return this.value;
-    }
+//     andThen<T2>(mapper: (value: T) => OptionalSome<T2>): OptionalSome<T2>;
+//     andThen(mapper: (value: T) => OptionalNone): OptionalNone;
+//     andThen<T2>(mapper: (value: T) => Optional<T2>): Optional<T2> {
+//         return mapper(this.value);
+//     }
+//     andThenAsync<T2>(mapper: (value: T) => Promise<OptionalSome<T2>>): Promise<OptionalSome<T2>>;
+//     andThenAsync(mapper: (value: T) => Promise<OptionalNone>): Promise<OptionalNone>;
+//     andThenAsync<T2>(mapper: (value: T) => Promise<Optional<T2>>): Promise<Optional<T2>> {
+//         return mapper(this.value);
+//     }
 
-    andThen<T2>(mapper: (value: T) => OptionalSome<T2>): OptionalSome<T2>;
-    andThen(mapper: (value: T) => OptionalNone): OptionalNone;
-    andThen<T2>(mapper: (value: T) => Optional<T2>): Optional<T2> {
-        return mapper(this.value);
-    }
-    andThenAsync<T2>(mapper: (value: T) => Promise<OptionalSome<T2>>): Promise<OptionalSome<T2>>;
-    andThenAsync(mapper: (value: T) => Promise<OptionalNone>): Promise<OptionalNone>;
-    andThenAsync<T2>(mapper: (value: T) => Promise<Optional<T2>>): Promise<Optional<T2>> {
-        return mapper(this.value);
-    }
+//     orElse(): OptionalSome<T> {
+//         return this;
+//     }
+//     async orElseAsync(): Promise<OptionalSome<T>> {
+//         return this;
+//     }
 
-    orElse(): OptionalSome<T> {
-        return this;
-    }
-    async orElseAsync(): Promise<OptionalSome<T>> {
-        return this;
-    }
+//     map<U>(mapper: (value: T) => U): OptionalSome<U> {
+//         return new OptionalSome(mapper(this.value))
+//     }
+//     async mapAsync<U>(mapper: (value: T) => Promise<U>): Promise<OptionalSome<U>> {
+//         return new OptionalSome(await mapper(this.value))
+//     }
 
-    map<U>(mapper: (value: T) => U): OptionalSome<U> {
-        return new OptionalSome(mapper(this.value))
-    }
-    async mapAsync<U>(mapper: (value: T) => Promise<U>): Promise<OptionalSome<U>> {
-        return new OptionalSome(await mapper(this.value))
-    }
-
-    toResult(): ResultValid<T> {
-        return new ResultValid(this.value)
-    }
-}
+//     toResult(): ResultValid<T> {
+//         return new ResultValid(this.value)
+//     }
+// }
